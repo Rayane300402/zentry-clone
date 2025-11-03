16da80d092842d49d7161f4dbaa25955d91ec718
Rayane300402 <raycaratboulsi@gmail.com>
2025-11-03 16:39:44 +0200

    finished about + started navbar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,7 @@ const Button = ({ title, id, leftIcon, rightIcon, containerClass }) => {
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
         <div>{title}</div>
       </span>
+      {rightIcon}
     </button>
   );
 };
