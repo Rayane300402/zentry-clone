a568879d30a451214801718d116e519853a8cc15
Rayane300402 <raycaratboulsi@gmail.com>
2025-11-17 18:44:33 +0200

    completed features + minor edits to index.css

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,6 +1,81 @@
+import type { ReactNode } from "react";
+import { useState, useRef } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+type CustomCardProps = {
+  src: string;
+  title: ReactNode;
+  description: string;
+};
+
+type CardTiltProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export const CardTilt = ({ children, className = "" }: CardTiltProps) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 10;
+    const tiltY = (relativeX - 0.5) * -8;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.98, .98, .98)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CustomCard = ({ src, title, description }: CustomCardProps) => {
+  
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-lavender-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Features = () => {
   return (
-    <section className="bg-black pb-52 ">
+    <section id="nexus" className="bg-black pb-52 ">
       <div className="container mx-auto px-3 md:px-10">
         <div className="px-5 py-32">
           <p className="font-circular-web text-lg text-lavender-50">
@@ -12,9 +87,75 @@ const Features = () => {
             experience on your world.
           </p>
         </div>
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <CardTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <CustomCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radie<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+          />
+        </CardTilt>
+
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7 ">
+          <CardTilt className="bento-tilt_1 col-span-2 row-span-1 md:col-span-1 md:row-span-2">
+            <CustomCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  zig<b>m</b>a
+                </>
+              }
+              description="An anime and gaming-inspired NFT collectioin - the IP primed for expansion"
+            />
+          </CardTilt>
+          <CardTilt className="bento-tilt_1 col-span-2 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <CustomCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of social interaction for Web3 communities"
+            />
+          </CardTilt>
+
+          <CardTilt className="bento-tilt_1 col-span-2 me-14 md:col-span-1 md:me-0">
+            <CustomCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            />
+          </CardTilt>
+
+          <CardTilt className="bento-tilt_2">
+              <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+                <h1 className="bento-title special-font max-w-64 text-black">M<b>o</b>re co<b>m</b>i<b>n</b>g s<b>oo</b>n</h1>
+                <TiLocationArrow className="m-5 scale-[5] self-end"/>
+              </div>
+          </CardTilt>
+
+          <CardTilt className="bento-tilt_2">
+              <video 
+               src="videos/feature-5.mp4"
+               loop
+               muted
+               autoPlay
+               className="size-full object-cover object-center"
+              />
+          </CardTilt>
 
         </div>
+
+
+
       </div>
     </section>
   );
